2944b857d3a31b416a33ae6936286cff84272819
Sanmi <dukesanmi@gmail.com>
2024-02-21 16:31:09 +0100

    Set up server

---
+++ index.js
@@ -0,0 +1,37 @@
+const log = console.log;
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const ejs = require('ejs');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const date = new Date();
+const PORT = process.env['PORT'] || 8000;
+
+// Env variables
+require('dotenv').config();
+
+const app = express();
+
+// CORS Config
+app.use(cors({
+    origin: "*"
+}));
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// File Upload
+app.use(fileUpload({
+    useTempFiles: true,
+    limits: {limits: 50 * 2024 * 1024}
+}));
+
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+//App Listen
+app.listen(PORT, log(`Server run on PORT ${PORT}, Date: ${date}`));
