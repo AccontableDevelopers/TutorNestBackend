fa3f471e00b3c82ffa785542bf92c5a088939d98
Dayo Farayola <dayojnr.3@gmail.com>
2024-03-04 10:21:45 +0100

    feat: create new passoword feature

---
+++ services/tutor.services.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const bcrypt = require("bcryptjs")
+const bodyParser = require("body-parser")
+const User = require("../models/Tutor")
+
+const app = express();
+
+app.use(bodyParser.json())
+
+const createNewPassword = async (payload) => {
+    const { newPassword } = payload;
+
+    try {
+        const foundTutor = await User.findById({ _id: payload._id })
+
+        if (!foundTutor) return "No user with provided id";
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+        //update the tutors password
+        foundTutor.password = hashedPassword;
+
+        await foundTutor.save();
+
+        return "Password changed successfully!"
+
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+module.exports = { createNewPassword }
