1e10c769f63f4dacbeccbbe2c7221c91937539e5
Iupac120 <oforokpara@gmail.com>
2024-04-03 13:08:47 +0100

    added utils

--- utils/errorHandlers.js
+++ utils/errorHandlers.js
@@ -1,33 +1,58 @@
-class BadUserRequestError extends Error {
-    constructor(message){
-      super(message)
-      this.status = 400;
-      this.errorType = "BadUserRequestError";
-    }
+// class BadUserRequestError extends Error {
+//     constructor(message){
+//       super(message)
+//       this.status = 400;
+//       this.errorType = "BadUserRequestError";
+//     }
+// }
+
+// class NotFoundError extends Error {
+//   constructor(message){
+//     super(message)
+//     this.status = 404;
+//     this.errorType = "NotFoundError";
+//   }
+// }
+
+// class UnAuthorizedError extends Error {
+//   constructor(message){
+//     super(message)
+//     this.status = 401;
+//     this.errorType = "UnAuthorizedError";
+//   }
+// }
+
+// class FailedRequestError extends Error {
+//   constructor(message){
+//     super(message)
+//     this.status = 500;
+//     this.errorType = "FailedRequestError";
+//   }
+// }
+
+
+
+function createError(status, errorType, message) {
+  const error = new Error(message);
+  error.status = status;
+  error.errorType = errorType;
+  return error;
 }
 
-class NotFoundError extends Error {
-  constructor(message){
-    super(message)
-    this.status = 404;
-    this.errorType = "NotFoundError";
-  }
+function BadUserRequestError(message) {
+  return createError(400, "BadUserRequestError", message);
 }
 
-class UnAuthorizedError extends Error {
-  constructor(message){
-    super(message)
-    this.status = 401;
-    this.errorType = "UnAuthorizedError";
-  }
+function NotFoundError(message) {
+  return createError(404, "NotFoundError", message);
 }
 
-class FailedRequestError extends Error {
-  constructor(message){
-    super(message)
-    this.status = 500;
-    this.errorType = "FailedRequestError";
-  }
+function UnAuthorizedError(message) {
+  return createError(401, "UnAuthorizedError", message);
+}
+
+function FailedRequestError(message) {
+  return createError(500, "FailedRequestError", message);
 }
 
 module.exports = {
@@ -35,4 +60,4 @@ module.exports = {
   NotFoundError,
   UnAuthorizedError,
   FailedRequestError
-}
+};
